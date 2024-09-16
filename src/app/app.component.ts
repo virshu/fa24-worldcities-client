94093e80a5d82c8e58c1ce3f2743243f5e138fca
virshu <felix@rabinovich.org>
2024-09-15 19:17:43 -0700

    Connect to server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,5 +17,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public forecasts: WeatherForecast[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getForecasts();
+  }
+
+  getForecasts() {
+    this.http.get<WeatherForecast[]>('http://localhost:5288/weatherforecast').subscribe({
+      next: result => this.forecasts = result,
+      error: e => console.error(e)
+      })
+  }
+ 
   title = 'AngClient';
 }
